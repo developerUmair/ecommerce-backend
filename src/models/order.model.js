49c88981e5e5234408b398680e68b10835c2adc1
developerUmair <umairmak143@gmail.com>
2025-03-28 23:27:28 +0500

    order mode created

---
+++ src/models/order.model.js
@@ -0,0 +1,109 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [1, "Quantity cannot be less than 1"],
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  selectedVariant: {
+    type: String,
+  },
+});
+
+const shippingAddressSchema = new mongoose.Schema({
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+const paymentResultSchema = new mongoose.Schema({
+  id: { type: String },
+  status: { type: String },
+  update_time: { type: String },
+  email_address: { type: String },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    orderItems: [orderItemSchema],
+    shippingAddress: shippingAddressSchema,
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: paymentResultSchema,
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
